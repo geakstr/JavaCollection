1d7317f328e66508e3d7eb20170962d4461b1562
Dmitry Kharitonov <geakstr@Dmitrys-iMac.local>
2013-09-24 20:38:31 +0400

    Replacement of the old Pair implementation of the new

--- src/me/geakstr/algo/string/hash/Pair.java
+++
@@ -1,7 +0,0 @@
-package me.geakstr.algo.string.hash;
-
-import java.util.ArrayList;
-
-class Pair {
-	public ArrayList<Integer> a, b;
-}
--- src/me/geakstr/algo/string/hash/Pows.java
+++ src/me/geakstr/algo/string/hash/Pows.java
@@ -1,17 +1,20 @@
 package me.geakstr.algo.string.hash;
 
+import me.geakstr.util.Pair;
+
 import java.util.ArrayList;
 
 class Pows {
 	private static ArrayList<Integer> p1 = null, p2 = null;
-	private static Pair pair = null;
+	private static Pair<ArrayList<Integer>, ArrayList<Integer>> pair = null;
 
-	public static Pair get(int n, int w1, int m1, int w2, int m2) {
+	public static Pair<ArrayList<Integer>, ArrayList<Integer>> get(int n,
+			int w1, int m1, int w2, int m2) {
 		int size = p1 == null ? 0 : p1.size();
 		int start = size;
 		if (p1 == null || size == 0) {
 			start = 1;
-			pair = new Pair();
+			pair = new Pair<ArrayList<Integer>, ArrayList<Integer>>();
 			p1 = new ArrayList<Integer>();
 			p2 = new ArrayList<Integer>();
 			p1.add(start);
